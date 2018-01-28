d8a8c0bdb538e2669eddcd7ab044f233ac2a8cef
Aaron Muntean <tenix109@gmail.com>
2018-01-28 12:54:41 -0500

    Created the ProductController and added the 4 ActionMethods: Index(), Browse(), Details(), and Location().

---
+++ Lab2/Lab2/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+			return "Product/Index is displayed";
+        }
+
+		public string Browse()
+		{
+			return "Browse displayed";
+		}
+
+		public string Details(int id)
+		{
+			return "Details displayed for Id="+id;
+		}
+
+		public string Location(string zip)
+		{
+			string message = WebUtility.HtmlEncode("Location displayed for zip=" + zip);
+
+			return message;
+		}
+    }
+}
