ae4bb8f9637be3be42734bf716fb9cde7fde31dc
Aaron Muntean <s00973717@acad.tri-c.edu>
2018-03-12 23:06:52 -0400

    Created a new MVC project with individual user authentication.  Made the entire website require authentication except for the Home Index. Added Google authentication.

---
+++ WebApplication_IndividualAuth/WebApplication_IndividualAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_IndividualAuth.Startup))]
+namespace WebApplication_IndividualAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
