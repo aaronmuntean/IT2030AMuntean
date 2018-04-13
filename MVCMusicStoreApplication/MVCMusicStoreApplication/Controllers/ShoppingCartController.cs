5dcafe3eff5eac7a043b61c60f099e1319c229bd
Aaron Muntean <s00973717@acad.tri-c.edu>
2018-04-13 00:00:56 -0400

    Added shopping cart functionality and views for adding and removing items from the shopping cart. Also displays quantities and total cost..

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/ShoppingCartController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+using MVCMusicStoreApplication.Models.ViewModels;
+
+namespace MVCMusicStoreApplication.Controllers {
+    public class ShoppingCartController : Controller {
+		MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: ShoppingCart
+        public ActionResult Index() {
+			ShoppingCart cart = ShoppingCart.GetCart(this.HttpContext);
+			ShoppingCartViewModel vm = new ShoppingCartViewModel() {
+				CartItems = cart.GetCartItems(),
+				CartTotal = cart.GetCartTotal()
+			};
+
+			return View(vm);
+        }
+
+		// GET: ShoppingCart/AddToCart/5
+		public ActionResult AddToCart(int id) {
+			ShoppingCart cart = ShoppingCart.GetCart(this.HttpContext);
+			cart.AddToCart(id);
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		//POST: Ajax Call
+		public ActionResult RemoveFromCart(int id) {
+			//id == RecordId
+			ShoppingCart cart = ShoppingCart.GetCart(this.HttpContext);
+
+			string albumTitle = db.Carts.SingleOrDefault(c => c.RecordId == id).AlbumSelected.Title;
+
+			int count = cart.RemoveFromCart(id);
+
+			ShoppingCartRemoveViewModel vm = new ShoppingCartRemoveViewModel() {
+				ItemCount = count,
+				DeleteId = id,
+				CartTotal = cart.GetCartTotal(),
+				Message = albumTitle + " has been removed from the cart."
+			};
+
+			return Json(vm);
+		}
+    }
+}
---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models.ViewModels {
+	public class ShoppingCartRemoveViewModel {
+		public int DeleteId { get; set; } //Match with RecordId
+		public decimal CartTotal { get; set; }
+		public int ItemCount { get; set; }
+		public int CartCount { get; set; }
+		public string Message { get; set; }
+	}
+}
